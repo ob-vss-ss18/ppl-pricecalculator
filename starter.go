4797841e2ceab18e52009d38a001a4f519615068
Unknown <matthias.herpers@gmail.com>
2018-06-05 09:45:15 +0200

    FirstTest

--- starter.go
+++ starter.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"strings"
+	"strconv"
+	"math"
 )
 
 func main() {
@@ -21,4 +24,32 @@ func main() {
 
 func hello(res http.ResponseWriter, req *http.Request) {
 	fmt.Fprintln(res, "hello, world - DEV")
+	// WERTE abfrage über Item id
+	//
+	maxGebraucht := 149.0;
+	maxNeu := 249.0;
+	preis := 139.0
+	testWert := 50.0;
+	serviceFlat := 49.0;
+	versicherung := 30.0;
+	factor := 0.36;
+	rabattFamilie := 0.10;
+	rabattSaison := 0.10;
+	gebraucht := false
+	zusatz := 0.0 //stöcke oder so
+
+	zwischenErgebnis := 100.0;
+	result := zwischenErgebnis + (testWert +serviceFlat + versicherung  +
+		(preis* factor) + zusatz ) * (1.0-rabattFamilie) * (1.0-rabattSaison)
+
+	if (gebraucht && result > maxGebraucht){
+		result = maxGebraucht
+	}
+	if (!gebraucht && result > maxNeu){
+		result = maxNeu
+	}
+
+	fmt.Println(res,"Result = %d",result);
+
+
 }
