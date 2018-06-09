415fd3745be1911e94d81ac2d39a5907dc8904c2
mountainflo <tobusch@hm.edu>
2018-06-09 13:20:31 +0200

    refactor static price calculation #9

---
+++ calulation.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"net/http"
+	"fmt"
+)
+
+/*
+	calculation price with static values. Display price in browser.
+ */
+func calculateExamplePrice(res http.ResponseWriter, req *http.Request) {
+
+
+
+	fmt.Fprintln(res, "hello, world - DEV")
+	// WERTE abfrage über Item id
+	//
+	//feste Werte
+	maxGebraucht := 149.0
+	maxNeu := 249.0
+	testWert := 50.0	// Wert von Testen von anderen Skis
+	serviceFlat := 49.0
+	versicherung := 30.0
+
+	//eingaben
+	neuwert := 439.0
+	amortisationFactor := 0.36
+	rabattFamilie := 0.10
+	rabattSaison := 0.10
+	gebraucht := false
+	zusatz := 0.0 //stoecke etc.
+
+	zwischenErgebnis := neuwert * amortisationFactor
+
+	if gebraucht && zwischenErgebnis > maxGebraucht{
+		zwischenErgebnis = maxGebraucht
+	}
+	if !gebraucht && zwischenErgebnis > maxNeu{
+		zwischenErgebnis = maxNeu
+	}
+
+	result := (zwischenErgebnis + serviceFlat + versicherung +
+		zusatz) * (1.0 - rabattFamilie) * (1.0 - rabattSaison)
+	reference := neuwert + testWert + serviceFlat + versicherung + zusatz
+
+	fmt.Fprintln(res,"Preis = ",result)
+
+	fmt.Fprintln(res,"Referenz = ", reference)
+
+}
--- starter.go
+++ starter.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/", hello)
+	http.HandleFunc("/", calculateExamplePrice)
 	fmt.Println("listening...")
 	port := os.Getenv("PORT")
 	if port == "" {
@@ -17,46 +17,4 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-}
-
-func hello(res http.ResponseWriter, req *http.Request) {
-
-
-
-	fmt.Fprintln(res, "hello, world - DEV")
-	// WERTE abfrage über Item id
-	//
-	//feste Werte
-	maxGebraucht := 149.0
-	maxNeu := 249.0
-	testWert := 50.0	// Wert von Testen von anderen Skis
-	serviceFlat := 49.0
-	versicherung := 30.0
-
-	//eingaben
-	neuwert := 439.0
-	amortisationFactor := 0.36
-	rabattFamilie := 0.10
-	rabattSaison := 0.10
-	gebraucht := false
-	zusatz := 0.0 //stoecke etc.
-
-	zwischenErgebnis := neuwert * amortisationFactor
-
-	if gebraucht && zwischenErgebnis > maxGebraucht{
-		zwischenErgebnis = maxGebraucht
-	}
-	if !gebraucht && zwischenErgebnis > maxNeu{
-		zwischenErgebnis = maxNeu
-	}
-
-	result := (zwischenErgebnis + serviceFlat + versicherung +
-		 zusatz) * (1.0 - rabattFamilie) * (1.0 - rabattSaison)
-	reference := neuwert + testWert + serviceFlat + versicherung + zusatz
-
-	fmt.Fprintln(res,"Preis = ",result)
-
-	fmt.Fprintln(res,"Referenz = ", reference)
-
-
 }
